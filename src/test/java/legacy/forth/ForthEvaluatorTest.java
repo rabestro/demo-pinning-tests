1725f067e968b2b9fc909c791f85ff6edae92df7
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-11-23 21:44:39 +0200

    Add ForthEvaluatorTest and update pom.xml
    Added a new test class ForthEvaluatorTest.java in the legacy.forth package. The pom.xml file was updated with changes including lowering the Java version from 21 to 17 and adding the PMD dependency with version 6.55.0. Adjustments were made in the plugins to include maven-pmd-plugin and reorder others. The purpose of these changes is to improve the maintainability and ensure adherence to coding standards in the project.

---
+++ src/test/java/legacy/forth/ForthEvaluatorTest.java
@@ -0,0 +1,13 @@
+package legacy.forth;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ForthEvaluatorTest {
+
+    @Test
+    void dump() {
+        assertTrue(true);
+    }
+}
